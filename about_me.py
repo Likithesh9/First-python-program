81db79a7c97978ccaa4f380de51e59d855ffe0a4
Likithesh Henjar ss <162932822+Likithesh9@users.noreply.github.com>
2025-04-27 09:23:43 +0530

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,10 @@
+my_intro= "Hi,my name is Likithesh."
+my_age="I am 20 years old."
+my_height="I am 5.7 feet tall."
+my_occupation="I am currently a student."
+my_greeting="Nice to meet you!"
+print(my_intro)
+print(my_age)
+print(my_height)
+print(my_occupation)
+print(my_greeting)
